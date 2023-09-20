727739c506f2a2b5936e0668e0ea41c652b4ce0a
Jannik Schröder <jannik.schroeder05@gmail.com>
2023-09-20 17:53:05 +0200

    initial comit with finished stuff

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.9-slim
+
+# Arbeitsverzeichnis setzen
+WORKDIR /app
+
+# Abhängigkeiten installieren
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Anwendungscode kopieren
+COPY . .
+
+
+# Den Dienst auf Port 5000 starten
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, request, jsonify
+import paramiko
+
+app = Flask(__name__)
+
+API_KEY = os.environ.get("API_KEY", "default_api_key") 
+
+
+PRIVATE_KEY_PATH = os.environ.get("PRIVATE_KEY_PATH", "/root/.ssh/id_rsa")
+
+
+
+SERVERS = {
+    "kb-web-prod-1": "10.0.0.120",
+    "kb-web-prod-2": "10.0.0.122",
+    "kb-dash-prod-1": "10.0.0.124",
+    "kb-dash-prod-2": "10.0.0.126",
+    "kb-api-prod-1": "10.0.0.128",
+    "kb-api-prod-2": "10.0.0.130",
+    "kb-webhook-handler": "10.0.0.132",
+    "kb-web-dev": "10.0.0.134",
+    "kb-dash-dev": "10.0.0.136",
+    "kb-api-dev": "10.0.0.138"
+}
+
+
+def execute_ssh_command(host, command):
+    try:
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, username='your_ssh_user', key_filename=PRIVATE_KEY_PATH)
+        
+        stdin, stdout, stderr = client.exec_command(command)
+        result = stdout.read().decode('utf-8')
+        client.close()
+        return result
+    except Exception as e:
+        return f"Error: {e}"
+
+@app.route('/update/<server_name>', methods=['POST'])
+def webhook_handler(server_name):
+    api_key = request.headers.get('API-Key')
+
+    if not api_key or api_key != API_KEY:
+        return jsonify(error="Invalid API Key"), 401
+
+    if server_name not in SERVERS:
+        return jsonify(error="Invalid server name"), 400
+
+    output = execute_ssh_command(SERVERS[server_name], 'docker-compose pull && docker-compose up -d')
+    return jsonify(output=output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
